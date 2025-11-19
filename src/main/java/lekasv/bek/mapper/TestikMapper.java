09de291fe9682002c8f404b9fef90ac7b71f3cb6
Elena-kontaurova <kontaurova0306@gmail.com>
2025-11-20 00:24:33 +0300

    куку

--- src/main/java/lekasv/bek/mapper/TestikMapper.java
+++ src/main/java/lekasv/bek/mapper/TestikMapper.java
@@ -1,8 +1,8 @@
 package lekasv.bek.mapper;
 
 import lekasv.bek.dto.testik.CreateTestikRequest;
-import lekasv.bek.dto.testik.UpdateTestikRequest;
 import lekasv.bek.dto.testik.TestikResponse;
+import lekasv.bek.dto.testik.UpdateTestikRequest;
 import lekasv.bek.model.Testik;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingTarget;
